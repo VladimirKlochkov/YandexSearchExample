991a984377f6bbfcfd91ae3331adcd0f8a650f22
v_klo_000 <v_klochkov@list.ru>
2018-08-04 17:09:58 +0300

    Разработка тестового задания.

--- src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
+++ src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
@@ -1,39 +1,27 @@
 package ru.StepDefinitions;
 
-import com.codeborne.selenide.Configuration;
+
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 
-public class YandexTextSearchStepDefinitions {
-  private String keyword;
-  
-  @Given("an open browser with google.com")
-  public void openGoogleSearch() {
-    Configuration.browser = "chrome";
-    Configuration.startMaximized = false;
-    Configuration.reportsFolder = "target/surefire-reports";
-    open("https://google.com/ncr");
-  }
+/**
+ * Класс описывающий шаги теста по поиску электричек с помощью соответствующего сервиса Яндекса.
+ * Created by Vladimir V. Klochkov on 04.08.2018.
+ * Updated by Vladimir V. Klochkov on 04.08.2018.
+ */
+public class YandexTextSearchStepDefinitions
+{
+  /*******************************************************************************************************************
+   * Поля класса.
+   ******************************************************************************************************************/
 
-  @When("a keyword (.*) is entered in input field")
-  public void enterKeyword(String keyword) {
-    this.keyword = keyword;
-    $(By.name("q")).val(keyword).pressEnter();
-  }
-
-  @Then("at least top (\\d+) matches should be shown")
-  public void topTenMatchesShouldBeShown(int resultsCount) {
-    $$("#ires .g").shouldHave(sizeGreaterThanOrEqual(resultsCount));
-  }
-
-  @Then("the first one should contain (.+)")
-  public void theFirstOneShouldContainKeyword(String expectedText) {
-    $("#ires .g").shouldHave(text(expectedText));
+  /*******************************************************************************************************************
+   * Методы класса.
+   ******************************************************************************************************************/
+  @Given("^Пользователь открывает страницу \"([^\"]*)\"$")
+  public void userOpensPageByUrl(String url)
+  {
+      open(url);
   }
 }
