5181ddeff8ef3265166e59b49517f15257a58bd9
v_klo_000 <v_klochkov@list.ru>
2018-08-04 18:16:57 +0300

    Разработка тестового задания.

--- src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
+++ src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
@@ -2,6 +2,7 @@ package ru.StepDefinitions;
 
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 import org.junit.Assert;
 import ru.PageObjects.YandexMainPage;
 
@@ -23,11 +24,17 @@ public class YandexTextSearchStepDefinitions
    * Методы класса.
    ******************************************************************************************************************/
   @Given("^Пользователь открывает страницу 'yandex.ru'$")
-  public void userOpensPageByUrl()
+  public void userOpensYandexMainPage()
   {
       String url = "https://yandex.ru";
 
       yandexMainPage.openYandexMainPage(url);
       Assert.assertTrue("Главная страница Яндекса не открылась", url().contains(url));
+  }
+
+  @When("^Пользователь переходит на страницу сервиса 'Яндекс Расписания'$")
+    public void userGoesToYandexTimetablesPage()
+  {
+
   }
 }
