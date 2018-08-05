5c06d3a369516a79a9c099dd140b764c22447cee
v_klo_000 <v_klochkov@list.ru>
2018-08-05 12:11:26 +0300

    Разработка тестового задания.

--- src/main/java/ru/PageObjects/YandexMainPage.java
+++ src/main/java/ru/PageObjects/YandexMainPage.java
@@ -2,18 +2,16 @@ package ru.PageObjects;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.url;
 
 /**
  * Класс для работы с главной страницей Яндекса (https://yandex.ru/).
  * Created by Vladimir V. Klochkov on 04.08.2018.
- * Updated by Vladimir V. Klochkov on 04.08.2018.
+ * Updated by Vladimir V. Klochkov on 05.08.2018.
  */
 public class YandexMainPage
 {
@@ -44,7 +42,6 @@ public class YandexMainPage
     {
         open(url);
         yandexMainSearchString.waitUntil(clickable, timeout, polling);
-        Assert.assertTrue("Главная страница Яндекса не открылась", url().contains(url));
 
         return this;
     }
--- src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
+++ src/test/java/ru/StepDefinitions/YandexTextSearchStepDefinitions.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.WebDriverRunner.url;
 /**
  * Класс описывающий шаги теста по поиску электричек с помощью соответствующего сервиса Яндекса.
  * Created by Vladimir V. Klochkov on 04.08.2018.
- * Updated by Vladimir V. Klochkov on 04.08.2018.
+ * Updated by Vladimir V. Klochkov on 05.08.2018.
  */
 public class YandexTextSearchStepDefinitions
 {
@@ -29,6 +29,7 @@ public class YandexTextSearchStepDefinitions
       String url = "https://yandex.ru";
 
       yandexMainPage.openYandexMainPage(url);
+      Assert.assertTrue("Главная страница Яндекса не открылась", url().contains(url));
   }
 
   @When("^Пользователь переходит на страницу сервиса 'Яндекс Расписания'$")
