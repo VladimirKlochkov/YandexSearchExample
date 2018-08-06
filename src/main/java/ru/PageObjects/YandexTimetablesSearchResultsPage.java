20aca55d403ddc43fe46025dc623816af85c81e2
v_klo_000 <v_klochkov@list.ru>
2018-08-06 16:16:29 +0300

    Разработка тестового задания.

--- src/main/java/ru/PageObjects/YandexTimetablesSearchResultsPage.java
+++ src/main/java/ru/PageObjects/YandexTimetablesSearchResultsPage.java
@@ -2,11 +2,6 @@ package ru.PageObjects;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
-import java.util.Hashtable;
 import static com.codeborne.selenide.Selenide.$;
 
 /**
@@ -21,27 +16,11 @@ public class YandexTimetablesSearchResultsPage extends YandexMainPage
      * Локаторы элементов страницы.
      ******************************************************************************************************************/
     //------------------------------------------------------------------------------------------------------------------
-    // Позиция переключателя [Электричка]
-    private SelenideElement electricTrain =
-            $(By.xpath("//div[@class='header__transport-selector']//label[contains(., 'Электричка')]"));
+    // Заголовок страницы в котором содержатся параметры поиска [Пункт отправления] и [Пункт назначения]
+    private SelenideElement fromToNames = $(By.xpath("//h1/span"));
     //------------------------------------------------------------------------------------------------------------------
-    // Строка для текстового поиска [Пункт отправления]
-    private SelenideElement fromName =
-            $(By.xpath("//input[@class='station-input_search__control' and @name='fromName']"));
-    //------------------------------------------------------------------------------------------------------------------
-    // Строка для текстового поиска [Пункт назначения]
-    private SelenideElement toName = $(By.xpath("//input[@class='station-input_search__control' and @name='toName']"));
-    //------------------------------------------------------------------------------------------------------------------
-    // Пиктограмма открытия календаря для поля [Когда]
-    private SelenideElement datePickerSearchIcon = $(By.xpath("//label[@class='datepicker_search__icon']"));
-    //------------------------------------------------------------------------------------------------------------------
-    // Шаблон для поиска дня недели в открытом календаре по дате
-    private String calendarDay =
-            "//div[(@class='calendar__day' or @class='calendar__day _weekend') and @data-date='%s']";
-    //------------------------------------------------------------------------------------------------------------------
-    // Кнопка [Найти]
-    private SelenideElement searchButton =
-            $(By.xpath("//button[@type='submit']/span[@class='y-button_islet-rasp-search__text']"));
+    // Заголовок страницы в котором содержится параметр поиска [Когда]
+    private SelenideElement dayOfWeek = $(By.xpath("//span[@class='SearchTitle__subtitle']"));
     //------------------------------------------------------------------------------------------------------------------
 
     /*******************************************************************************************************************
@@ -53,42 +32,6 @@ public class YandexTimetablesSearchResultsPage extends YandexMainPage
      */
     public YandexTimetablesSearchResultsPage switchToElectricTrain()
     {
-        electricTrain.waitUntil(clickable, timeout, polling).click();
-
         return this;
-    }
-
-    /**
-     * Осуществляет поиск электричек в заданном направлении.
-     * @param from пункт отправления
-     * @param to пункт назначения
-     * @param when день недели или дата в виде строки
-     */
-    public void searchForTimeTable(String from, String to, String when)
-    {
-        Hashtable<String, DayOfWeek> days = new Hashtable<>();
-        days.put("понедельник", DayOfWeek.MONDAY);
-        days.put("вторник", DayOfWeek.TUESDAY);
-        days.put("среда", DayOfWeek.WEDNESDAY);
-        days.put("четверг", DayOfWeek.THURSDAY);
-        days.put("пятница", DayOfWeek.FRIDAY);
-        days.put("суббота", DayOfWeek.SATURDAY);
-        days.put("воскресенье", DayOfWeek.SUNDAY);
-
-        // Получаем дату в формате ГГГГ-ММ-ДД для того, чтобы найти требуемый элемент в календаре
-        LocalDate nextDayOfWeek = LocalDate.now().with(TemporalAdjusters.next(days.get(when)));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String nextDayOfWeekString = nextDayOfWeek.format(formatter);
-
-        fromName.waitUntil(clickable, timeout, polling).click();
-        fromName.clear();
-        fromName.sendKeys(from);
-        toName.waitUntil(clickable, timeout, polling).click();
-        toName.clear();
-        toName.sendKeys(to);
-        datePickerSearchIcon.waitUntil(clickable, timeout, polling).click();
-        SelenideElement day = $(By.xpath(String.format(calendarDay, nextDayOfWeekString)));
-        day.waitUntil(clickable, timeout, polling).click();
-        searchButton.waitUntil(clickable, timeout, polling).click();
     }
 }
