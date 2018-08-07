49386cca98c891a153d5fe750f51ffb8a2a90b2c
v_klo_000 <v_klochkov@list.ru>
2018-08-07 16:27:56 +0300

    Разработка тестового задания.

--- src/main/java/ru/PageObjects/YandexTripDataDetailsPage.java
+++ src/main/java/ru/PageObjects/YandexTripDataDetailsPage.java
@@ -25,15 +25,16 @@ public class YandexTripDataDetailsPage extends YandexMainPage
     // Название таблицы [Пункт назначения]
     private SelenideElement tableHeaderToName = $(By.xpath("//h1[@class='b-page-title__title']/span[5]"));
     //------------------------------------------------------------------------------------------------------------------
-    // Полное название рейса в легенде календаря
-    private SelenideElement legendTrip = $(By.xpath("//div[@class='b-page-calendar-legend__trip']/a"));
-    //------------------------------------------------------------------------------------------------------------------
     // Таблица маршрута (общая часть локатора)
     private String timeTable = "//table[@class='b-timetable i-bem b-timetable_js_inited']/tbody/";
     //------------------------------------------------------------------------------------------------------------------
     // Таблица маршрута [Пункт отправления]
     private SelenideElement timeTableFromName = $(By.xpath(timeTable +
             "tr[@class='b-timetable__row b-timetable__row_type_start']//a"));
+    //------------------------------------------------------------------------------------------------------------------
+    // Таблица маршрута [Время отправления]
+    private SelenideElement timeTableFromTime = $(By.xpath(timeTable +
+            "tr[@class='b-timetable__row b-timetable__row_type_start']//strong"));
     //------------------------------------------------------------------------------------------------------------------
 
     /*******************************************************************************************************************
